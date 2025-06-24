583884360466a62881b8ab0fb99d4d750ee24954
r1xez <164931719+r1xez@users.noreply.github.com>
2025-06-24 17:56:07 +0300

    completed

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+class CafeWorker
+{
+    public string Name { get; set; }
+    public string Position { get; set; }
+
+    public CafeWorker(string name, string position)
+    {
+        Name = name;
+        Position = position;
+    }
+
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"{Position}: {Name}");
+    }
+}
+
+
+class Cafe : IEnumerable<CafeWorker>
+{
+    private List<CafeWorker> workers = new List<CafeWorker>();
+
+    public void AddWorker(CafeWorker worker)
+    {
+        workers.Add(worker);
+    }
+
+    public IEnumerator<CafeWorker> GetEnumerator()
+    {
+        return workers.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cafe myCafe = new Cafe();
+
+        myCafe.AddWorker(new CafeWorker("Olena", "Barista"));
+        myCafe.AddWorker(new CafeWorker("Andriy", "Chef"));
+        myCafe.AddWorker(new CafeWorker("Iryna", "Waitress"));
+
+        Console.WriteLine("Cafe Staff:");
+        foreach (var worker in myCafe)
+        {
+            worker.DisplayInfo();
+        }
+    }
+}
